a19d789051a1f6b92457ce2da2ffe934b0568bdb
cam292 <cam292@pitt.edu>
2018-03-20 17:49:00 -0400

    Added LibraryModule

---
+++ study-connect/src/app/library/library.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//basically all declarations should be exports of this module
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+
+  ],
+  exports: [
+
+  ],
+  providers: [
+
+  ]
+})
+export class LibraryModule { }
