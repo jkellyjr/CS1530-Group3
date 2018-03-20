f1a0281bf47cd36b3131fc4ca66ede05e2cc4d34
cam292 <cam292@pitt.edu>
2018-03-20 18:08:35 -0400

    Added DashboardComponent to UserModule

--- study-connect/src/app/user/user.module.ts
+++ study-connect/src/app/user/user.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserComponent } from './user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    UserComponent
+    UserComponent,
+    DashboardComponent
   ],
   exports: [
     UserComponent
