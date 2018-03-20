d21f1acc3bd23868361ecc8ffbc4f3c061e91eb9
cam292 <cam292@pitt.edu>
2018-03-20 17:32:29 -0400

    Added HomeModule

--- study-connect/src/app/app.module.ts
+++ study-connect/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
