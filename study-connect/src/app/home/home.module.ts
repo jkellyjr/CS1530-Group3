70aaa0cce732df75a44b0a733064b226ab120bd2
Alexander Patton <adp62@pitt.edu>
2018-03-28 18:36:03 -0400

    Created about us component

--- study-connect/src/app/home/home.module.ts
+++ study-connect/src/app/home/home.module.ts
@@ -5,16 +5,29 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    MatInputModule
+    MatInputModule,
+    MatTabsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     HomeComponent
--- study-connect/src/app/home/login/login.component.ts
+++ study-connect/src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isLinear = false;
+  loginGroup: FormGroup;
+  firstRegisterGroup: FormGroup;
+  secondRegisterGroup: FormGroup;
+  thirdRegisterGroup: FormGroup;
+  fourthRegisterGroup: FormGroup;
+  fifthRegisterGroup: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginGroup = this._formBuilder.group({
+      loginCtrl: ['', Validators.required]
+    })
+    this.firstRegisterGroup = this._formBuilder.group({
+      firstRegisterCtrl: ['', Validators.required]
+    });
+    this.secondRegisterGroup = this._formBuilder.group({
+      secondRegisterCtrl: ['', Validators.required]
+    });
+    this.thirdRegisterGroup = this._formBuilder.group({
+      thirdRegisterCtrl: ['', Validators.required]
+    });
   }
 
 }
