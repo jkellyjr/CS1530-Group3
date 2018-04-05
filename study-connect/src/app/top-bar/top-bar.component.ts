b0915ede0b7a20ada6352c6c7018cf01db6e7d5f
cam292 <cam292@pitt.edu>
2018-04-05 17:18:04 -0400

    Hide link icons if the user is not logged in yet

--- study-connect/src/app/top-bar/top-bar.component.ts
+++ study-connect/src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ISubscription } from 'rxjs/Subscription';
 
+import { AuthService } from '../auth/index';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
+  logged:Boolean;
+  loggedSubscription: ISubscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.loggedSubscription = this.authService.logged.subscribe(
+      logged => {
+          this.logged = logged;
+      })
   }
 
 }
