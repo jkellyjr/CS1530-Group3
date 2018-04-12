8ad315dc1fc520831b5cc20337e7856876189afc
cam292 <cam292@pitt.edu>
2018-04-12 16:11:25 -0400

    Fixed conversation object

--- study-connect/src/app/library/objects/Conversation.ts
+++ study-connect/src/app/library/objects/Conversation.ts
@@ -2,8 +2,11 @@ import { Message } from './index';
 export class Conversation {
   id: number;
   student_id: number;
+  student_name:string;
   tutor_id: number;
+  tutor_name:string;
   group_id: number;
+  group_name: string;
   messages: Message[];
 
   constructor(){
