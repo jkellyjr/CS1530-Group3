d062dbdb3d1b5bba7f9914d5c2cb2afe619c2181
cam292 <cam292@pitt.edu>
2018-04-12 15:07:29 -0400

    Updated user object, fixed table component

--- study-connect/src/app/library/objects/User.ts
+++ study-connect/src/app/library/objects/User.ts
@@ -3,6 +3,7 @@ import { Group } from './Group';
 import { Meeting } from './Meeting';
 import { Message } from './Message';
 import { RequestContact } from './RequestContact';
+import { Conversation } from './Conversation';
 
 export class User {
   id: number;
@@ -15,14 +16,24 @@ export class User {
   groups: Group[];
   current_courses: Course[];
   past_courses: Course[];
-  meetings: Meeting[];
-  contact_requests: RequestContact[];
+  // meetings: Meeting[];
+
+  tutor_conversations: Conversation[];
+  student_conversations: Conversation[];
+
+  tutor_contact_requests: RequestContact[];
+  student_contact_requests: RequestContact[];
 
   constructor(){
     this.groups = new Array<Group>();
     this.current_courses = new Array<Course>();
     this.past_courses = new Array<Course>();
-    this.meetings = new Array<Meeting>();
-    this.contact_requests = new Array<RequestContact>();
+    // this.meetings = new Array<Meeting>();
+
+    this.tutor_conversations = new Array<Conversation>();
+    this.student_conversations = new Array<Conversation>();
+
+    this.tutor_contact_requests = new Array<RequestContact>();
+    this.student_contact_requests = new Array<RequestContact>();
   }
 }
