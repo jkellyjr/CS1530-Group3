0c864724fa56bb267eca01e30aea0ad47119b8ae
Evan Gutman <evangutman12@gmail.com>
2018-04-12 12:38:37 -0400

    Reslove Merge conflict

--- study_connect_api/models.py
+++ study_connect_api/models.py
@@ -178,6 +178,9 @@ class Course(db.Model):
 
     # course_tutor = db.relationship('Tutor', backref = db.backref('course', lazy = True))
 
+    #added for meeting request ***
+    meeting_requests = db.relationship('MeetingRequest', backref = db.backref('Course', lazy = True))
+
     def serialize(self):
         return {
             "id":self.id,
@@ -244,6 +247,10 @@ class Conversation(db.Model):
     tutor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = True)
 
     messages = db.relationship('Message', backref = db.backref('Conversation', lazy = True))
+
+    #added for meeting request***
+    meeting_requests = db.relationship('MeetingRequest', backref = db.backref('Conversation', lazy = True))
+
 
     def serialize(self):
         map = { "id": self.id }
@@ -351,6 +358,15 @@ class ContactRequest(db.Model):
         return '<ContactRequest %r, %r>' % (self.requestor_id, self.message)
 
 
+class MeetingRequest(db.Model):
+    __tablename__ = 'meeting_request'
+
+    id = db.Column(db.Integer, primary_key = True, unique = True)
+    date = db.Column(db.DateTime, nullable = False)
+    location = db.Column(db.String(30), nullable = True)
+    
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
 
 # class Tutor(db.Model):
 #     __tablename__ = 'tutor'
@@ -405,3 +421,53 @@ class ContactRequest(db.Model):
 #
 #     def __repr__(self):
 #         return "<Ratings {}>".format(self.reviwer, self.reviewee, self.stars, slef.rate_tutor, self.rate_student, self.comments, self.rating_time)
+<<<<<<< Updated upstream
+=======
+
+
+class Meeting(db.Model):
+    __tablename__ = 'meeting'
+
+    id = db.Column(db.Integer, unique = True, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    meeting_time = db.Column(db.DateTime, nullable = False)
+    location = db.Column(db.String(50), nullable = True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    tutor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def serialize(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'meeting_time':dump_datetime(self.meeting_time),
+            'location':self.location
+            # 'group':self.group.serialize(),
+            # 'student':self.student.serialize(),
+            # 'tutor':self.tutor.serialize()
+        }
+
+    def __init__(self, name, meeting_time, location, student, group, tutor):
+        self.name = name
+        self.meeting_time = meeting_time
+        self.location = location
+        self.student_id = student
+        self.group_id = group
+        self.tutor_id = tutor
+
+    def __repr__(self):
+        return "<Meetings %r >" % (self.name)
+
+
+
+
+class MeetingRequest(db.Model):
+    __tablename__ = 'meeting_request'
+
+    id = db.Column(db.Integer, unique = True, primary_key = True)
+    conversation_id(db.Foreignkey
+
+
+
+>>>>>>> Stashed changes
