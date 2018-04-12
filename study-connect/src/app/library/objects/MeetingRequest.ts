1a3d4ec7184554077cf57222cc518e3d7dd58194
cam292 <cam292@pitt.edu>
2018-04-12 16:26:36 -0400

    updated request object

--- study-connect/src/app/library/objects/MeetingRequest.ts
+++ study-connect/src/app/library/objects/MeetingRequest.ts
@@ -2,9 +2,12 @@
 export class MeetingRequest{
   constructor(
     public id:number,
+    public approved:boolean,
     public conversation_id: number,
-    public date: string,
+    public meeting_date: string,
     public location: string,
-    public course_id: number
+    public course_id: number,
+    public student_requestor_id: number,
+    public requested_name:string
   ){  }
 }
