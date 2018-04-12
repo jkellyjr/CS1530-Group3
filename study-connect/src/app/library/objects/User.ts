d781672913ffe7b04c41e8ce3e35e8357c4e6587
cam292 <cam292@pitt.edu>
2018-04-12 13:16:43 -0400

    Updated objects

--- study-connect/src/app/library/objects/User.ts
+++ study-connect/src/app/library/objects/User.ts
@@ -2,6 +2,7 @@ import { Course } from './Course';
 import { Group } from './Group';
 import { Meeting } from './Meeting';
 import { Message } from './Message';
+import { RequestContact } from './RequestContact';
 
 export class User {
   id: number;
@@ -15,11 +16,13 @@ export class User {
   current_courses: Course[];
   past_courses: Course[];
   meetings: Meeting[];
+  contact_requests: RequestContact[];
 
   constructor(){
     this.groups = new Array<Group>();
     this.current_courses = new Array<Course>();
     this.past_courses = new Array<Course>();
     this.meetings = new Array<Meeting>();
+    this.contact_requests = new Array<RequestContact>();
   }
 }
